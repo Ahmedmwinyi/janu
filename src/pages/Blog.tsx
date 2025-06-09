bf48e4000d0164326362f7e1124a2def3bb67506
Ahmedmwinyi <Madiylal@gmail.com>
2025-06-09 12:37:57 +0300

    Initial commit

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Calendar, ArrowRight, Clock, Tag, Search } from "lucide-react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
